cb04cfdcdd5f1e950314022b51c9d1e5d0bdac1a
Cogentx360 Labs <cogentx@Seans-MacBook-Pro.local>
2020-09-09 15:55:45 -0230

    Building Header-01

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,9 +12,18 @@ function Header() {
       </div>
 
       <div className="header__nav">
-        <div className="header__option"></div>
-        <div className="header__option"></div>
-        <div className="header__option"></div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
+        <div className="header__option">
+        <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+        <div className="header__option">
+        <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
       </div>
     </div>
   )

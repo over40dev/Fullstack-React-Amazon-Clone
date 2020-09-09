c730a801da0ea628c161eb326feb1835fd163c85
Cogentx360 Labs <cogentx@Seans-MacBook-Pro.local>
2020-09-09 15:00:03 -0230

    Amazon-Clone | React - Firebase (initial commit)

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBPJvyXvvdocBQxUTFZf2RzkHnbiEC-iVs",
+  authDomain: "launchpad-clones.firebaseapp.com",
+  databaseURL: "https://launchpad-clones.firebaseio.com",
+  projectId: "launchpad-clones",
+  storageBucket: "launchpad-clones.appspot.com",
+  messagingSenderId: "531439066742",
+  appId: "1:531439066742:web:41b9137fa83c0e34cf0957",
+  measurementId: "G-CXSTTC10SK"
+};

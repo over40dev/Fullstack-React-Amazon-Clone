43023e7fcfcb7425a7c7dae46f6c4e915ccf6675
Cogentx360 Labs <cogentx@Seans-MacBook-Pro.local>
2020-09-11 17:20:44 -0230

    Add Routing with React-Router-DOM

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import './App.css';
 import React from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     // BEM
-    <div className="App">
-      <Header/>
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Header/>
+            <h1>I AM CHECKOUT, SMASH THE LIKE BUTTON</h1>
+          </Route>
+          <Route path="/">
+            <Header/>
+            <Home/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

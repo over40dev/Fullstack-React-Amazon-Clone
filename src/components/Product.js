2df7b620be9bf9d294590367354f8d4a19000903
over40dev <ted@over40.dev>
2020-09-13 16:38:43 -0230

    Add to Basket working

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import {useStateValue} from '../State/StateProvider';
 import './Product.css';
 
-function Product({title, image, price, rating}) {
+function Product({id, title, image, price, rating}) {
+
+  const [{basket}, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    console.table(basket);
+    // dispatch the item into the data layer
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item: {
+        id,
+        title,
+        image,
+        price,
+        rating
+      }
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -13,14 +32,14 @@ function Product({title, image, price, rating}) {
         <div className="product__rating">
           {Array(rating)
             .fill()
-            .map(() => (
-              <span role="img" aria-label="star ratings">
+            .map((_, i) => (
+              <span key={i} role="img" aria-label="star ratings">
                 ⭐</span>
             ))}
         </div>
       </div>
       <img src={image} alt={title}/>
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
